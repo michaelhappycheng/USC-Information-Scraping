8a865a7f8e21e618d9bdb0e8f8a957a8935e57c8
Samuel Breck <breck@usc.edu>
2017-01-10 01:15:14 -0800

    scrape dining halls yus

--- sUSCDining/USCDining/spiders/USCDinning_Spider.py
+++ sUSCDining/USCDining/spiders/USCDinning_Spider.py
@@ -39,8 +39,8 @@ class USCDining_Spider(BaseSpider):
 
       currUrl = response.request.url
       currDate = currUrl.replace("http://hospitality.usc.edu/residential-dining-menus/?menu_venue=venue-507&menu_date=", "").replace("%2F", "/")
-      currDate = currUrl.replace("http://hospitality.usc.edu/residential-dining-menus/?menu_venue=venue-514&menu_date=", "").replace("%2F", "/")
-      currDate = currUrl.replace("http://hospitality.usc.edu/residential-dining-menus/?menu_venue=venue-518&menu_date=", "").replace("%2F", "/")
+      currDate = currDate.replace("http://hospitality.usc.edu/residential-dining-menus/?menu_venue=venue-514&menu_date=", "").replace("%2F", "/")
+      currDate = currDate.replace("http://hospitality.usc.edu/residential-dining-menus/?menu_venue=venue-518&menu_date=", "").replace("%2F", "/")
 
       # Both of these work
       hxs = HtmlXPathSelector(response)
